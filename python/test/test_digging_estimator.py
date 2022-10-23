86ccdfb5e976e5b7dc4537e11974dc82e5bcadc5
TMIRANDE <mirande1501@gmail.com>
2022-10-23 20:10:44 +0200

    "test min or max miner team day and night"

--- python/test/test_digging_estimator.py
+++ python/test/test_digging_estimator.py
@@ -13,7 +13,6 @@ class DiggingEstimatorTest(unittest.TestCase):
         result = estimator.tunnel(28, 2, "Granite")
         assert result.total == 48
 
-
     def test_error_negative_day(self):
 
         estimator = DiggingEstimator()
@@ -21,13 +20,38 @@ class DiggingEstimatorTest(unittest.TestCase):
         with pytest.raises(InvalidFormatException):
            estimator.tunnel(28, -100, "Granite")
 
-
     def test_error_impossible_digging(self):
 
         estimator = DiggingEstimator()
         estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
         with pytest.raises(TunnelTooLongForDelayException):
             estimator.tunnel(50, 1, "Granite")
+
+    def test_max_dt_miner_day(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(28, 2, "Granite")
+        assert result.day_team.miners == 3
+
+    def test_max_dt_miner_night(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(28, 2, "Granite")
+        assert result.night_team.miners == 3
+
+    def test_min_dt_miner_day(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(20, 2000, "Granite")
+        assert result.day_team.miners == 0
+
+    def test_min_dt_miner_night(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(20, 2000, "Granite")
+        assert result.night_team.miners == 0
+
+
 
 
 
