18fe37026ff4f7575a400f7cabeb1feb7d339e70
TMIRANDE <mirande1501@gmail.com>
2022-10-23 20:28:48 +0200

    "test min or max for team day and night"

--- python/test/test_digging_estimator.py
+++ python/test/test_digging_estimator.py
@@ -6,6 +6,7 @@ import pytest
 
 class DiggingEstimatorTest(unittest.TestCase):
 
+
     def test_returns_as_doctor_Pockosky_says(self):
 
         estimator = DiggingEstimator()
@@ -27,29 +28,45 @@ class DiggingEstimatorTest(unittest.TestCase):
         with pytest.raises(TunnelTooLongForDelayException):
             estimator.tunnel(50, 1, "Granite")
 
-    def test_max_dt_miner_day(self):
+    def test_max_dw_miner_dt(self):
         estimator = DiggingEstimator()
         estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
         result = estimator.tunnel(28, 2, "Granite")
         assert result.day_team.miners == 3
 
-    def test_max_dt_miner_night(self):
+    def test_max_dw_miner_nt(self):
         estimator = DiggingEstimator()
         estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
         result = estimator.tunnel(28, 2, "Granite")
         assert result.night_team.miners == 3
 
-    def test_min_dt_miner_day(self):
+    def test_min_dw_miner_dt(self):
         estimator = DiggingEstimator()
         estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
         result = estimator.tunnel(20, 2000, "Granite")
         assert result.day_team.miners == 0
 
-    def test_min_dt_miner_night(self):
+    def test_min_dw_miner_dn(self):
         estimator = DiggingEstimator()
         estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
         result = estimator.tunnel(20, 2000, "Granite")
         assert result.night_team.miners == 0
+
+
+    def test_max_dw_compo_total_dt(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(28, 2, "Granite")
+        total_dt = result.day_team.miners + result.day_team.healers + result.day_team.smithies + result.day_team.lighters + result.day_team.inn_keepers + result.day_team.guards + result.day_team.guard_managers + result.day_team.washers
+        assert total_dt == 16
+
+    def test_max_dw_compo_total_nt(self):
+        estimator = DiggingEstimator()
+        estimator.get = MagicMock(return_value=[0, 3, 5.5, 7])
+        result = estimator.tunnel(28, 2, "Granite")
+        total_nt = result.night_team.miners + result.night_team.healers + result.night_team.smithies + result.night_team.lighters + result.night_team.inn_keepers + result.night_team.guards + result.night_team.guard_managers + result.night_team.washers
+        assert total_nt == 32
+
 
 
 
