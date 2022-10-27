6693ec9f8389a15f2b81b162f414b318646651d5
fhemery <f.hemery@hemit.fr>
2022-10-27 17:13:56 +0200

    add night team miners to tests

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,13 +1,13 @@
 import { DiggingEstimator, InvalidFormatException, TunnelTooLongForDelayException } from "./digging-estimator";
 import { RockInformationInterface } from "./rock-information.interface";
 
-const ONE_DWARF_DIG_PER_DAY = 3
-const TWO_DWARVES_DIG_PER_DAY = 5.5
-const THREE_DWARVES_DIG_PER_DAY = 7
+const ONE_DWARF_DIG_PER_ROTATION = 3
+const TWO_DWARVES_DIG_PER_ROTATION = 5.5
+const THREE_DWARVES_DIG_PER_ROTATION = 7
 
 export class FakeRockInformationService implements RockInformationInterface {
   get(): number[] {
-    return [0, ONE_DWARF_DIG_PER_DAY, TWO_DWARVES_DIG_PER_DAY, THREE_DWARVES_DIG_PER_DAY];
+    return [0, ONE_DWARF_DIG_PER_ROTATION, TWO_DWARVES_DIG_PER_ROTATION, THREE_DWARVES_DIG_PER_ROTATION];
   }
 }
 const GRANITE = "granite";
@@ -37,20 +37,50 @@ describe("digging estimator", () => {
   });
 
   it(`should request one day miner when there is note more than 3 meters to dig`, function() {
-    const result = estimator.tunnel(ONE_DWARF_DIG_PER_DAY * 2, 2, GRANITE);
+    const result = estimator.tunnel(ONE_DWARF_DIG_PER_ROTATION * 2, 2, GRANITE);
 
     expect(result.dayTeam.miners).toBe(1);
   });
 
-  it(`should request two day miners for ${TWO_DWARVES_DIG_PER_DAY} to dig per day`, function() {
-    const result = estimator.tunnel(Math.floor(TWO_DWARVES_DIG_PER_DAY * 3), 3, GRANITE);
+  it(`should request two day miners for ${TWO_DWARVES_DIG_PER_ROTATION} to dig per day`, function() {
+    const result = estimator.tunnel(Math.floor(TWO_DWARVES_DIG_PER_ROTATION * 3), 3, GRANITE);
 
     expect(result.dayTeam.miners).toBe(2);
   });
 
-  it(`should request three day miners for ${THREE_DWARVES_DIG_PER_DAY} to dig per day`, function() {
-    const result = estimator.tunnel(Math.floor(THREE_DWARVES_DIG_PER_DAY * 3), 3, GRANITE);
+  it(`should request three day miners for ${THREE_DWARVES_DIG_PER_ROTATION} to dig per day`, function() {
+    const result = estimator.tunnel(Math.floor(THREE_DWARVES_DIG_PER_ROTATION * 3), 3, GRANITE);
 
     expect(result.dayTeam.miners).toBe(3);
+  });
+
+  it(`should not request any night miner for less than ${THREE_DWARVES_DIG_PER_ROTATION} to dig per day`, function() {
+    const result = estimator.tunnel(Math.floor(THREE_DWARVES_DIG_PER_ROTATION * 3), 3, GRANITE);
+
+    expect(result.nightTeam.miners).toBe(0);
+  });
+
+  it(`should request one night miner for additional ${ONE_DWARF_DIG_PER_ROTATION} to dig per day`, function() {
+    const distanceToDig = Math.floor(THREE_DWARVES_DIG_PER_ROTATION + ONE_DWARF_DIG_PER_ROTATION);
+
+    const result = estimator.tunnel(distanceToDig , 1, GRANITE);
+
+    expect(result.nightTeam.miners).toBe(1);
+  });
+
+  it(`should request two night miners for additional ${TWO_DWARVES_DIG_PER_ROTATION} to dig per day`, function() {
+    const distanceToDig = Math.floor(THREE_DWARVES_DIG_PER_ROTATION + TWO_DWARVES_DIG_PER_ROTATION);
+
+    const result = estimator.tunnel(distanceToDig , 1, GRANITE);
+
+    expect(result.nightTeam.miners).toBe(2);
+  });
+
+  it(`should request three night miners for additional ${THREE_DWARVES_DIG_PER_ROTATION} to dig per day`, function() {
+    const distanceToDig = Math.floor(THREE_DWARVES_DIG_PER_ROTATION + THREE_DWARVES_DIG_PER_ROTATION);
+
+    const result = estimator.tunnel(distanceToDig , 1, GRANITE);
+
+    expect(result.nightTeam.miners).toBe(3);
   });
 });
