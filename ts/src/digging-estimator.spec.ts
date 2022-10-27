614a69856f39fc0d0941149a3fb669c4dc0987d6
fhemery <f.hemery@hemit.fr>
2022-10-27 15:14:25 +0200

    add validation of distance to dig

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,4 +1,4 @@
-import { DiggingEstimator, InvalidFormatException } from "./digging-estimator";
+import { DiggingEstimator, InvalidFormatException, TunnelTooLongForDelayException } from "./digging-estimator";
 import { RockInformationInterface } from "./rock-information.interface";
 
 export class FakeRockInformationService implements RockInformationInterface {
@@ -6,6 +6,7 @@ export class FakeRockInformationService implements RockInformationInterface {
     return [0, 3, 5.5, 7];
   }
 }
+const GRANITE = "granite";
 
 describe("digging estimator", () => {
   let estimator: DiggingEstimator;
@@ -15,15 +16,19 @@ describe("digging estimator", () => {
   });
 
   it("should return as Dr Pockovsky said", () => {
-    const result = estimator.tunnel(28, 2, "granite");
+    const result = estimator.tunnel(28, 2, GRANITE);
 
     expect(result.total).toBe(48);
   });
 
   it("should check days and length are positive integers", function() {
-    expect(() => estimator.tunnel(-1, 2, "granite")).toThrow(new InvalidFormatException());
-    expect(() => estimator.tunnel(2.5, 2, "granite")).toThrow(new InvalidFormatException());
-    expect(() => estimator.tunnel(2, -1, "granite")).toThrow(new InvalidFormatException());
-    expect(() => estimator.tunnel(2, 5.01, "granite")).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(-1, 2, GRANITE)).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(2.5, 2, GRANITE)).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(2, -1, GRANITE)).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(2, 5.01, GRANITE)).toThrow(new InvalidFormatException());
+  });
+
+  it("should return exception if tunnel is too long to be dug during delay", function() {
+    expect(() => estimator.tunnel(30, 2, GRANITE)).toThrow(new TunnelTooLongForDelayException());
   });
 });
